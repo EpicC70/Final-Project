2a96f0058528667d2d833a44ac22da3e69507461
EpicC70 <ahinkley@student.fitchburgstate.edu>
2023-04-24 20:39:53 -0400

    Update CameraControl.cs

--- Assets/Scripts/CameraControl.cs
+++ Assets/Scripts/CameraControl.cs
@@ -17,12 +17,12 @@ public class CameraControl : MonoBehaviour
 
         float sensitivity = 0.05f;
         Vector3 vp = mycam.ScreenToViewportPoint(new Vector3(Input.mousePosition.x, Input.mousePosition.y, mycam.nearClipPlane));
-        vp.x -= 0.75f;
-        vp.y -= 0.75f;
+        vp.x -= 0.5f;
+        vp.y -= 0.5f;
         vp.x *= sensitivity;
         vp.y *= sensitivity;
-        vp.x += 0.75f;
-        vp.y += 0.75f;
+        vp.x += 0.5f;
+        vp.y += 0.5f;
         Vector3 sp = mycam.ViewportToScreenPoint(vp);
 
         Vector3 v = mycam.ScreenToWorldPoint(sp);
